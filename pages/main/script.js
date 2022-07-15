609ad6ad7c1b9fb694d4374954b5c3f5ceaccdd4
Ilia Dvorchuk <vseprid@gmail.com>
2022-07-15 18:16:22 +0300

    feat: add async function getData

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,3 +1,13 @@
 import { burger, switchNav } from '../../script/nav.js'
 
 burger.addEventListener('click' ,() => switchNav());
+
+const url = '/script/pets.json'
+
+async function getData(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+}
+
+getData(url)
